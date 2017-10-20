703e87044d18c47f8c2e767f5ced65ef884d8c55
Aayush <agarwal.aayush13@gmail.com>
2017-10-20 16:50:50 +0530

    first commit

---
+++ groups/views.py
@@ -0,0 +1,91 @@
+from django.contrib import messages
+from datetime import *
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
+
+from django.core.urlresolvers import reverse
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404,render,HttpResponse
+from django.views import generic
+from groups.models import Group,GroupMember,Events
+from . import models
+from groups.forms import *
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ("name", "description")
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
+
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single",kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group,slug=self.kwargs.get("slug"))
+
+        try:
+            GroupMember.objects.create(user=self.request.user,group=group)
+
+        except IntegrityError:
+            messages.warning(self.request,("Warning, already a member of {}".format(group.name)))
+
+        else:
+            messages.success(self.request,"You are now a member of the {} group.".format(group.name))
+
+        return super().get(request, *args, **kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single",kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+
+            membership = models.GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get("slug")
+            ).get()
+
+        except models.GroupMember.DoesNotExist:
+            messages.warning(
+                self.request,
+                "You can't leave this group because you aren't in it."
+            )
+        else:
+            membership.delete()
+            messages.success(
+                self.request,
+                "You have successfully left this group."
+            )
+        return super().get(request, *args, **kwargs)
+
+def addEvent(request):
+    if request.method == 'POST':
+        form = EventsForm(data=request.POST)
+        if form.is_valid:
+            event = form.save()
+            event.save()
+            return render(request,"test.html")
+        else:
+            print(form.errors)
+    else:
+        form = EventsForm()
+        return render(request,"addEvent.html",{'form':form})
+
+def showEvents(request):
+    now = datetime.now()
+    start = now
+    end = now + timedelta(days=4)
+    events = Events.objects.filter(end_date__range=(start.date(),end.date()))
+    return render(request,"showEvents.html",{'events':events})
